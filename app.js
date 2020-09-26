a4609a6ab75ae41c077942dd7042aa0f00e03617
Maxim Kistanov <maxim.kistanov@gmail.com>
2020-09-26 19:36:27 +0300

    added div feature

--- app.js
+++ app.js
@@ -96,6 +96,10 @@ function pressSum(sumButton) {
       case '*':
          sum = Number(outputTop.value) * Number(outputBottom.value);
          break;
+
+      case '*':
+         sum = Number(outputTop.value) / Number(outputBottom.value);
+         break;
    }
    
    outputTop.value = '0';
